c722918019181a88085ecd082a9ffb6f5a4a0612
joeyjira <joey.jira@gmail.com>
2017-07-18 10:25:00 -0700

    completed backend auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,30 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+    params[:user][:username],
+    params[:user][:password]
+    )
+
+    if @user
+      sign_in(@user)
+      render "api/users/show"
+    else
+      render(
+      json: ["Invalid Username/Password"],
+      status: 401
+      )
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      sign_out
+      render "static_pages/root"
+    else
+      render(
+      json: ["No User currently signed in!"],
+      status: 404
+      )
+  end
+end
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/entry.jsx",
+  entry: "./frontend/tell_tales.jsx",
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"

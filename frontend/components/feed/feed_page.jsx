27004ed16faefbe80f4d3ad1f21d5d95ea0f673e
joeyjira <joey.jira@gmail.com>
2017-07-22 15:31:18 -0700

    75% styling for discover page

--- frontend/components/feed/feed_page.jsx
+++ frontend/components/feed/feed_page.jsx
@@ -30,19 +30,19 @@ class FeedPage extends React.Component {
         <div className="main-feed">
           <GreetingContainer />
           <div className="feed-page-holder">
-            feed page holder
+            <div className="page-tagline">
+              <img src="http://res.cloudinary.com/llventioll/image/upload/v1500651465/Screen_Shot_2017-07-21_at_8.32.00_AM_ouxoye.png"/>
+              <h1>Hello World!</h1>
+            </div>
             <div className="search-bar-container">
-              <input type="text" value="searchbar" />
+              <input type="text" placeholder="Discover" />
             </div>
             <div className="rss-feed-result">
-              feed result
               <Switch>
                 <Route path="/feed/result" component={SourceContainer} />
                 <Route path="/feed/source/:sourceId" component={ArticleListContainer} />
               </Switch>
-
               <div className="rss-list">
-                rss-list
               </div>
             </div>
           </div>

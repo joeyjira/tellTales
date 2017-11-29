8beab9532a63dff7e216ab0ab1143e013930e929
Joey Jirasevijinda <joey.jira@gmail.com>
2017-11-28 23:54:20 -0800

    adjusted title position

--- frontend/components/session_form/slideShow.jsx
+++ frontend/components/session_form/slideShow.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class SlideShow extends React.Component {
     render() {
         const { title, imageSrc, display } = this.props
-        console.log(imageSrc)
+
         return(
             <div className="slide-show" style={display}>
                 <h1>{title}</h1>
